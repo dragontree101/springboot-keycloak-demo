ec0e6706a43d5850aedc6b957a608b261d50704c
longzhe <dragonlong1986@126.com>
2016-08-01 19:01:33 +0800

    update

---
+++ keycloak-service/src/main/java/com/dragon/study/springboot/keycloak/Application.java
@@ -0,0 +1,15 @@
+package com.dragon.study.springboot.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by dragon on 16/6/19.
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Application.class, args);
+  }
+}
---
+++ keycloak-service/src/main/java/com/dragon/study/springboot/keycloak/config/AccessTokenConfiguration.java
@@ -0,0 +1,25 @@
+package com.dragon.study.springboot.keycloak.config;
+
+import org.keycloak.KeycloakPrincipal;
+import org.keycloak.representations.AccessToken;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by dragon on 16/6/19.
+ */
+@Configuration
+public class AccessTokenConfiguration {
+
+  @Bean
+  @ConditionalOnMissingBean
+  public AccessToken getAccessToken() {
+    HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+    return ((KeycloakPrincipal) request.getUserPrincipal()).getKeycloakSecurityContext().getToken();
+  }
+}
