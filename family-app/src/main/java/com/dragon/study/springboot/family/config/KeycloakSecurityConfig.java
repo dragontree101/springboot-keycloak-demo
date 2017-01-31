990c82f8256396daeb87d1023b01dba7496c163e
longzhe <dragonlong1986@126.com>
2017-01-31 13:48:22 +0800

    添加一些配置文件

---
+++ family-app/src/main/java/com/dragon/study/springboot/family/config/KeycloakSecurityConfig.java
@@ -0,0 +1,67 @@
+package com.dragon.study.springboot.family.config;
+
+import org.keycloak.adapters.springsecurity.KeycloakSecurityComponents;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+/**
+ * Created by dragon on 16/6/19.
+ */
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackageClasses = KeycloakSecurityComponents.class)
+public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    auth.authenticationProvider(keycloakAuthenticationProvider());
+  }
+
+  @Bean
+  @Override
+  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+    return new NullAuthenticatedSessionStrategy();
+  }
+
+  @Bean
+  public FilterRegistrationBean keycloakAuthenticationProcessingFilterRegistrationBean(
+      KeycloakAuthenticationProcessingFilter filter) {
+    FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+    registrationBean.setEnabled(false);
+    return registrationBean;
+  }
+
+  @Bean
+  public FilterRegistrationBean keycloakPreAuthActionsFilterRegistrationBean(
+      KeycloakPreAuthActionsFilter filter) {
+    FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+    registrationBean.setEnabled(false);
+    return registrationBean;
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    super.configure(http);
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .sessionAuthenticationStrategy(sessionAuthenticationStrategy()).and()
+        .addFilterBefore(keycloakPreAuthActionsFilter(), LogoutFilter.class)
+        .addFilterBefore(keycloakAuthenticationProcessingFilter(), X509AuthenticationFilter.class)
+        .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint()).and()
+        .authorizeRequests().antMatchers("/family/*").hasRole("USER").antMatchers("/admin/*")
+        .hasRole("ADMIN").anyRequest().permitAll();
+  }
+}
