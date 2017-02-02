084340589a229966ce6e9c2a5ed22e129bd28efb
longzhe <dragonlong1986@126.com>
2017-02-02 22:16:21 +0800

    增加了router和store

--- family-js/src/main.js
+++ family-js/src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
+import store from './store'
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const routes = [{
+  path : '/',
+  component : App
+}];
+
+const router = new VueRouter({
+  routes
+});
+
+router.beforeEach((to, from, next) => {console.log("route before")});
 
 new Vue({
   el: '#app',
+  router,
+  store,
   render: h => h(App)
 })
---
+++ family-js/src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex);
+
+const state = {
+  auth: {}
+};
+
+export default new Vuex.Store({
+  state
+})
