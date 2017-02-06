cd589be6832827385eec4bcc24b0bfc1c33f542c
longzhe <dragonlong1986@126.com>
2017-02-06 11:42:04 +0800

    调整了一些结构

--- family-js/src/store/actions.js
+++ family-js/src/store/actions.js
@@ -1,7 +1,15 @@
 import * as types from './mutation-types'
+import Keycloak from 'keycloak-js';
 
 export default {
-  checkUserIsLogin({commit}) {
-    commit(types.CHECK_USER_IS_LOGIN)
+  checkUserIsLogin({commit, state}) {
+    if(state.auth.loggedIn) {
+      var keycloakAuth = new Keycloak('keycloak.json');
+      keycloakAuth.init({ onLoad: 'login-required' }).success(function () {
+        commit(types.CHECK_USER_IS_LOGIN, keycloakAuth)
+      }).error(function () {
+        alert("failed to login");
+      });
+    }
   }
 };
--- family-js/src/store/mutations.js
+++ family-js/src/store/mutations.js
@@ -1,17 +1,9 @@
 import * as types from './mutation-types'
-import Keycloak from 'keycloak-js';
 
 export default {
-  [types.CHECK_USER_IS_LOGIN] (state) {
-    if(!state.auth.loggedIn) {
-      var keycloakAuth = new Keycloak('keycloak.json');
-      keycloakAuth.init({ onLoad: 'login-required' }).success(function () {
-        state.auth.loggedIn = true;
-        state.auth.authz = keycloakAuth;
-        state.auth.logoutUrl = keycloakAuth.authServerUrl + "/realms/" + keycloakAuth.realm + "/protocol/openid-connect/logout?redirect_uri=http://localhost:8089/index.html"
-      }).error(function () {
-        alert("failed to login");
-      });
-    }
+  [types.CHECK_USER_IS_LOGIN] (state, keycloakAuth) {
+    state.auth.loggedIn = true;
+    state.auth.authz = keycloakAuth;
+    state.auth.logoutUrl = keycloakAuth.authServerUrl + "/realms/" + keycloakAuth.realm + "/protocol/openid-connect/logout?redirect_uri=http://localhost:8089/index.html"
   }
 };
